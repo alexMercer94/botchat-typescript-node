4f50ea6d2c3a65118a69ba770310670c4263c092
alexMercer94 <alex.c.valera94@hotmail.com>
2020-09-25 12:58:21 -0500

    Added structure to resolve requests sequentially

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -4,32 +4,23 @@ import { Request, Response } from 'express';
 import { runQuery } from '../utils/dialogflow';
 import { sendMessage } from '../utils/twilio';
 
-@Controller('api/bot')
-export class BotController {
-    /**
-     * Function to send a message by POST
-     * Twilio uses this method when it receives a message from Whatsapp
-     * @param request
-     * @param response
-     */
+@Controller('api/message')
+export class MessageController {
     @Post()
-    private postMessage(request: Request, response: Response) {
-        // Here we get the message body, the number to which we're sending the message, and the number sending the message.
-        const { Body, To, From } = request.body;
+    private postSendMessage(request: Request, response: Response) {
+        const { body, to, from } = request.body;
         console.log('BotCTRL post message');
+        console.log({ body, to, from });
 
-        console.log({ Body, To, From });
-
-        // Here sent the received message to Dialogflow so that it can be identified against an Intent.
-        runQuery(Body, From)
+        runQuery(body, from)
             .then((result: any) => {
                 // console.log('Dialogflow response', result.fulfillmentText);
-
+                // console.log(result.fulfillmentMessages[0].text.text[0]);
                 result.fulfillmentMessages
                     .reduce(function (sequence: Promise<any>, value: any) {
                         return sequence
                             .then(function () {
-                                return sendMessage(From, To, value.text.text[0]);
+                                return sendMessage(to, from, value.text.text[0]);
                             })
                             .then((res: any) => {
                                 console.log('Twilio Response:', res);
@@ -42,22 +33,11 @@ export class BotController {
                     .then(() => {
                         console.log('Completed');
                     });
-
-                // It send the fulfillment text received back to our user via Twilio
-                /* sendMessage(From, To, result.fulfillmentText)
-                    .then((res) => {
-                        // console.log(res);
-                        console.log('Twilio Response:', res);
-                    })
-                    .catch((error) => {
-                        console.error('error is ', error);
-                        Logger.Err(error);
-                    }); */
             })
             .catch((error) => {
                 console.error('error is ', error);
                 Logger.Err(error);
             });
-        return response.status(200).send('SUCCESS');
+        return response.status(200).send({ msg: 'success' });
     }
 }
